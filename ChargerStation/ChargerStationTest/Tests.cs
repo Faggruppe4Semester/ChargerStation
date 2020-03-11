b4fb0f14038af92355e6161ad38cc831bf35d02c
Worly97 <50401784+Worly97@users.noreply.github.com>
2020-03-11 15:03:22 +0100

    Substitude Driller

--- ChargerStation/ChargerStationTest/Tests.cs
+++ ChargerStation/ChargerStationTest/Tests.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using ChargerStation;
 using NUnit.Framework;
+
+
 
 namespace ChargerStationTest
 {
     [TestFixture]
     public class StationControlTests
     {
+        private StationControl _uut;
+        private IDoor _door;
+        private IUsbCharger _usbCharger;
+        private IRfIdReader _idReader;
 
         [SetUp]
         public void SetUp()
         {
-
+            _door = Substitude.For<IDoor>();
         }
         [Test]
         public void Test1()
