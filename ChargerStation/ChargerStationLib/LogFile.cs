526521b521d72ab354a6a55818aab1454d7228c8
Worly97 <50401784+Worly97@users.noreply.github.com>
2020-03-11 14:39:44 +0100

    Made A logfile class

--- ChargerStation/ChargerStationLib/LogFile.cs
+++ ChargerStation/ChargerStationLib/LogFile.cs
@@ -1,15 +1,26 @@
-﻿namespace ChargerStation
+﻿using System;
+using System.IO;
+
+namespace ChargerStation
 {
     public class LogFile
     {
-        public void LogDoorLocked()
+        public void LogDoorLocked(int LockID)
         {
-            
+            StreamWriter W  = File.AppendText("Log.txt");
+            W.Write("Log entry: ");
+            W.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            W.WriteLine($"Charging station locked using RFID: {LockID}");
+            W.WriteLine("-------------------------------------------------------");
         }
 
-        public void LockDoorUnlocked()
+        public void LogDoorUnlocked(int LockID)
         {
-            
+            StreamWriter W = File.AppendText("Log.txt");
+            W.Write("Log entry: ");
+            W.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            W.WriteLine($"Charging station locked using RFID: {LockID}");
+            W.WriteLine("-------------------------------------------------------");
         }
     }
 }
