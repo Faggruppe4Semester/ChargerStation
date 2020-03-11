fb705366f96943b48e654a9d35330a6f8ee90773
Worly97 <50401784+Worly97@users.noreply.github.com>
2020-03-11 15:18:19 +0100

    Setup til tests done

--- ChargerStation/ChargerStationTest/Tests.cs
+++ ChargerStation/ChargerStationTest/Tests.cs
@@ -11,13 +11,18 @@ namespace ChargerStationTest
     {
         private StationControl _uut;
         private IDoor _door;
-        private IUsbCharger _usbCharger;
+        private IChargerControl _chargerControl;
         private IRfIdReader _idReader;
 
         [SetUp]
         public void SetUp()
         {
             _door = Substitute.For<IDoor>();
+            _chargerControl = Substitute.For<IChargerControl>();
+            _idReader = Substitute.For<IRfIdReader>();
+
+            _uut = new StationControl(_door, _chargerControl, _idReader);
+
         }
         [Test]
         public void Test1()
