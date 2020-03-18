b3e3d39d19da4091e6a26b1fa1f74bd60e29bc45
Matias Rosenkilde Tvorup <201807748@post.au.dk>
2020-03-18 14:01:37 +0100

    Fixet mit fuckup i Program.cs

--- ChargerStation/ChargerStation/Program.cs
+++ ChargerStation/ChargerStation/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             IDoor door = new Door();
-            IChargerControl c = new ChargerControl(new UsbChargerSimulator());
+            IChargerControl c = new ChargerControl(new UsbChargerSimulator(), new Display());
             IRfIdReader rfid = new RfIdReader();
             StationControl sc = new StationControl(door, c, rfid);
             //TODO: Eventhandleing in chargercontol
